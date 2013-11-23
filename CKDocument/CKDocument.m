676a320f171a2854a97e437e22b3b02029b6c857
Conrad Kreyling <konistehrad@gmail.com>
2013-11-23 11:29:12 -0500

    Fix duplicate license text

--- CKDocument/CKDocument.m
+++ CKDocument/CKDocument.m
@@ -3,9 +3,6 @@
 //  CKDocument
 //
 //  Created by Conrad Kreyling on 1/12/12.
-//  Copyright (c) 2012 Conrad Kreyling. All rights reserved.
-//
-//  Created by Conrad Kreyling on 1/12/12.
 //  Copyright (c) 2012 Conrad Kreyling. http://conradscloset.com
 //
 //  MIT LICENSE
